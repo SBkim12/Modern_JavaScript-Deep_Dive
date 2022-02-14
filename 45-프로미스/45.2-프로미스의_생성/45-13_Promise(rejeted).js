7752ce7723febba529fdec4a73a880fc3c9fc9ab
SBKim12 <mar5924@naver.com>
2022-02-15 00:50:01 +0900

    22/02/14 2-자바스크립트

--- 45-프로미스/45.2-프로미스의_생성/45-13_Promise(rejeted).js
+++ 45-프로미스/45.2-프로미스의_생성/45-13_Promise(rejeted).js
@@ -6,7 +6,10 @@ Promise {<rejected>: Error: error occurred
     at <anonymous>:1:52
     at new Promise (<anonymous>)
     at <anonymous>:1…}
-        [[Prototype]]: Promise
-        [[PromiseState]]: "rejected"
-        [[PromiseResult]]: Error: error occurred
+        [[Prototype]]: Promise                              
+        [[PromiseState]]: "rejected"                        <- 비동기 처리 상태 정보
+        [[PromiseResult]]: Error: error occurred            <- 비동기 처리 결과 정보
+
+
+=> 프로미스는 비동기 처리 상태와 처리 결과를 관리하는 객체
 */
